2187c2b3fd2ca5e14b6258876995b756412d71a8
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-07-21 10:28:16 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input onChange={(e) => setText(e.target.value)} />
+      <p>{text}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
