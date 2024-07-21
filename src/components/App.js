19af273a2a0e43ef6e5eb4ce51d695ad8e2f8185
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-21 10:29:11 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
+      <h3>Enter you name:</h3>
       <input onChange={(e) => setText(e.target.value)} />
       <p>{text}</p>
     </div>
